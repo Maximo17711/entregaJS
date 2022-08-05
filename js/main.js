85a132d8234f252d363d30c80efb26923732b14e
Maximo17711 <maximomazzocato493@gmail.com>
2022-08-04 23:48:23 -0300

    Primera entrega del proyecto final

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-for ( let i=1; i <= 5;) {
+for ( let i=1; i <= 99;) {
     let nombrePersona = prompt("ingrese su nombre para ser atendido");
     alert ("Turno Numero "+i+" Cliente: "+  nombrePersona)
     console.log("Cliente Numero: " + i)
@@ -6,17 +6,63 @@ for ( let i=1; i <= 5;) {
     break
 }
 
+//En objetos agregue la venta de una casa como si de una inmobiliaria se tratara
+
+
+const Casa1 ={
+    Nombre:"casa",
+    Accion:"comprar",
+    Localidad:"Avellaneda",
+    Garantia:["si","no"],
+    Valor:170000,
+    MetrosCuadrados:"150m2"
+
+    
+}
+
+//todo lo que se muestra en consola sera mostrado en un sistema de cartas donde estaran las propiedades a disposicion 
+
+console.log
+("la propiedad es una " + Casa1.Nombre)
+console.log
+("usted quiere " + Casa1.Accion)
+console.log
+("La localidad de su "+ Casa1.Nombre +" se encuetra en " + Casa1.Localidad)
+console.log
+("usted " + Casa1.Garantia[1] + " necesita garantia")
+console.log
+("La propiedad tiee un valor de $" + Casa1.Valor + " dolares")
+console.log
+("Cuenta con unos " + Casa1.MetrosCuadrados)
+
+//todos los valores tanto de la csaas como de los departamentos estan en dolares 
+
+const departamentos = [
+    {accion: 'comprar Chacarita', precio: 70000}, 
+    {accion: 'comprar San martin', precio: 120000},
+    {accion: 'alquilar Belgrano', precio:500},
+    {accion: 'alquilar Avellaneda', precio:980},
+]
+
+
+const resultado = departamentos.find((el) => el.accion === "comprar chacarita")
+
+console.log(resultado)
+//en este momento se le daria la opcion al cliente de que propiedad quiere elegir antes de que pase a la parte del valor del producto 
+
+//en la proxima entrega se sumara que el valor del producto pase directamente a ser pagado sin que le cliente tenga que tener alguna accion con la parte del valor del producto 
+
 //simulacion de un prestamo en cuotas, que se podria utilizar para un producto 
 
 function IngresoValor(){
-    let valor = parseInt(prompt("ingresar valor"));
+    let valor = parseInt(prompt("ingresar valor del producto"));
     alert("La cantidad es = " + valor)
     return valor
 }
 
 function NCuotas(){
-    let DivisionCuotas = parseInt(prompt("puede hacerlo hasta 12 cuotas, en cuantas las va a hacer?"));
-    alert("El prestamo sera en " + DivisionCuotas + " cuotas")
+    let DivisionCuotas = parseInt(prompt("puede hacerlo hasta 78 cuotas, en cuantas las va a hacer?"));
+    alert("Su propiedad se hara en " + DivisionCuotas + " cuotas")
     return DivisionCuotas
 }
 
@@ -34,4 +80,4 @@ console.log("Las cuotas son:" + DivisionCuotas)
 
 let division = dividir(valor, DivisionCuotas)
 
-console.log("Seran " + DivisionCuotas + " cuotas de $" + division)
+console.log("Seran " + DivisionCuotas + " cuotas de $" + division + " dolares por mes")
